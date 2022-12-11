1e78010db9137365f5e3470c64d75b4c2a767083
Takuya Motoshima <t-motoshima@showcase-tv.com>
2022-12-12 00:47:54 +0900

    Added language options to the web demo.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,15 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [1.0.3] - 2022/12/12
+### Changed
+- Added language options to the web demo.  
+    ![select-language-option.png](docs/web-demo/screencaps/select-language-option.png)
+
 ## [1.0.2] - 2022/12/9
-### Fixed
+### Added
 - Web demo added.
-  Click [here](docs/web-demo/README.md) to learn how to use the web demo.
+    Click [here](docs/web-demo/README.md) to learn how to use the web demo.
 
 ## [1.0.1] - 2022/11/10
 ### Fixed
@@ -16,3 +21,4 @@ All notable changes to this project will be documented in this file.
 
 [1.0.1]: https://github.com/takuya-motoshima/msoffice2pdf/compare/v1.0.0...v1.0.1
 [1.0.2]: https://github.com/takuya-motoshima/msoffice2pdf/compare/v1.0.1...v1.0.2
+[1.0.3]: https://github.com/takuya-motoshima/msoffice2pdf/compare/v1.0.2...v1.0.3
--- docs/web-demo/routes/convert.js
+++ docs/web-demo/routes/convert.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const msoffice2pdf = require('msoffice2pdf');
 const tmp = require('tmp');
+const {body, validationResult} = require('express-validator');
 
 function getExtensionFromMimeType(mimeType) {
   return {
@@ -21,9 +22,15 @@ function writeUploadedFileToTmp(buffer, extension) {
 }
 
 const router = express.Router();
-router.post('/', async (req, res, next) => {
+router.post('/', [
+  body('language').optional({nullable: true, checkFalsy: true}).isIn(['af', 'sq', 'ar-dz', 'ar-bh', 'ar-eg', 'ar-iq', 'ar-jo', 'ar-kw', 'ar-lb', 'ar-ly', 'ar-ma', 'ar-om', 'ar-qa', 'ar-sa', 'ar-sy', 'ar-tn', 'ar-ae', 'ar-ye', 'eu', 'be', 'bg', 'ca', 'zh-hk', 'zh-cn', 'zh-sg', 'zh-tw', 'hr', 'cs', 'da', 'nl-be', 'nl', 'en', 'en-au', 'en-bz', 'en-ca', 'en-ie', 'en-jm', 'en-nz', 'en-za', 'en-tt', 'en-gb', 'en-us', 'et', 'fo', 'fa', 'fi', 'fr-be', 'fr-ca', 'fr-lu', 'fr', 'fr-ch', 'gd', 'de-at', 'de-li', 'de-lu', 'de', 'de-ch', 'el', 'he', 'hi', 'hu', 'is', 'id', 'ga', 'it', 'it-ch', 'ja', 'ko', 'ko', 'ku', 'lv', 'lt', 'mk', 'ml', 'ms', 'mt', 'no', 'nb', 'nn', 'pl', 'pt-br', 'pt', 'pa', 'rm', 'ro', 'ro-md', 'ru', 'ru-md', 'sr', 'sk', 'sl', 'sb', 'es-ar', 'es-bo', 'es-cl', 'es-co', 'es-cr', 'es-do', 'es-ec', 'es-sv', 'es-gt', 'es-hn', 'es-mx', 'es-ni', 'es-pa', 'es-py', 'es-pe', 'es-pr', 'es', 'es-uy', 'es-ve', 'sv', 'sv-fi', 'th', 'ts', 'tn', 'tr', 'ua', 'ur', 've', 'vi', 'cy', 'xh', 'ji', 'zu',]),
+], async (req, res, next) => {
   try {
-    if (!req.files || !req.files.length)
+    const errs = validationResult(req);
+    if (!errs.isEmpty()) {
+      console.error('Form Validation Error:', errs.array());
+      return void res.status(400).json({errors: errs.array()});
+    } else if (!req.files || !req.files.length)
       throw new TypeError('files parameters are required');
     const file = req.files[0];
     const extension = getExtensionFromMimeType(file.mimetype);
@@ -32,7 +39,10 @@ router.post('/', async (req, res, next) => {
     const inputPath = writeUploadedFileToTmp(file.buffer, extension);
     const fileName = file.originalname.replace(/\.[^/.]+$/, '.pdf')
     const outputPath = `${global.APP_DIR}/public/upload/${fileName}`;
-    await msoffice2pdf(inputPath, outputPath);
+    const options = {};
+    if (req.body.language)
+      options.language = req.body.language;
+    await msoffice2pdf(inputPath, outputPath, options);
     res.json({filePath: outputPath.replace(`${global.APP_DIR}/public/`, '')});
   } catch (err) {
     next(err);
